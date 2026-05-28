f3824cb8697ae65f43acc6dd825ae150582b7f2c
Jon Bowie <bestballsaul@gmail.com>
2026-05-28 10:40:24 -0400

    Finished a few exercises

--- bash/allergies/allergies.sh
+++ bash/allergies/allergies.sh
@@ -22,3 +22,40 @@
 #   main "$@"
 #
 # *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+#
+#
+
+declare -a allergens
+allergens[1]="eggs"
+allergens[2]="peanuts"
+allergens[4]="shellfish"
+allergens[8]="strawberries"
+allergens[16]="tomatoes"
+allergens[32]="chocolate"
+allergens[64]="pollen"
+allergens[128]="cats"
+
+bit_indicator=1
+input=$1
+output=""
+allergic="false"
+
+while [[ $bit_indicator -lt 129 ]]; do
+	if ((input & bit_indicator)); then
+		output+="${allergens[$bit_indicator]} "
+	fi
+	((bit_indicator *= 2))
+done
+
+if [ "$2" = "list" ]; then
+	echo "${output% }"
+elif [ "$2" = "allergic_to" ]; then
+	if [[ $DEBUG = "true" ]]; then
+		echo "Output = ${output}"
+		echo "ARG 3 = $3"
+	fi
+	if [[ "${output% }" = *"$3"* ]]; then
+		allergic="true"
+	fi
+	echo ${allergic}
+fi
